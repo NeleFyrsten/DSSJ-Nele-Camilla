06abf83d8e84fb2d36322e2cbb9c09ce455de717
NeleFyrsten <152161825+NeleFyrsten@users.noreply.github.com>
2024-05-23 23:08:16 +0200

    Slider JS

--- slider.js
+++ slider.js
@@ -6,7 +6,7 @@
 
 // get images
 function getImages() {
-    fetch(images.json)
+    fetch("categorySlider.json")
     .then((response) => {
         if (!response.ok) {
             throw new Error(`HTTP Error! Status: ${response.status}`);
@@ -23,17 +23,104 @@ function getImages() {
 };
 
 function addImagesToSlider(data) {
-    for (const image of data) {
-        if (image.location === "categorySlider") { //check where the image is supposed to be shown (location), in this case categorySlider
-            const categorySliderCard = document.querySelectorAll('categorySlider__card');
+    // looking for the imageID of the categoryCard
+    const categorySliderCard = document.querySelectorAll('.categorySlider__card');
+    categorySliderCard.forEach(categoryCard => {
+        
+        const imageID = categoryCard.getAttribute('imageID');
+        
+        // looking for the image with the same id
+        for (const object of data) {
+            // getting the id of the object
+            const objectId = object.id;
+            console.log(objectId);
+            
+            if(imageID == objectId) {
+                console.log(`the element ${imageID} is connected to the image ${objectId}`)
+                // getting the img in the card
+                const categorySliderImage = categoryCard.querySelector('.categorySlider__image'); //categoryCard instead of document -> finds the first one in this section 
+                console.log(object.image);
+                categorySliderImage.src = `${object.image}`;
+                // getting the credit 
+                const imageCredit = categoryCard.querySelector('.imageCredit');
+                imageCredit.textContent = `Foto: ${object.imageCredit}`;
+                return //otherwise it continues checking the other ones, after it has found the right one
+            }
+        }
+    });
+}
+//calling the fetch function
+getImages()
 
 
-        } 
+// ----------- move the slider --------------
+
+//get the buttons and cards
+const sliderArrowPrev = document.querySelector('.slider__arrow--previous');
+const sliderArrowNext = document.querySelector('.slider__arrow--next');
+const sliderCard = document.querySelectorAll('.categorySlider__card');
+
+//events
+sliderArrowPrev.addEventListener('click', moveSlider);
+sliderArrowNext.addEventListener('click', moveSlider);
+sliderCard.forEach(card => {
+    card.addEventListener('click', moveSlider);
+})
+
+
+//imageID of the focus card in a variable -> we start with "all", which hast imageID=4
+let cardInFocus = 4;
+let cardCount = 0;
+
+//function: move the slider in the right direction and focus the right card
+function moveSlider(e) { //e = event the call comes from (either arrowPrev or arrowNext)
+    const slider = document.querySelector('.categorySlider');
+    console.log(e.target);
+
+    // stop the slider from moving too far, hiding the arrow in that direction
+    if (cardInFocus >= 8 || cardInFocus <= 1) {
+        e.currentTarget.style.opacity = "0";
+        return
     }
 
+    // if you click on a card instead of an arrow
+    //currentTarget: where it came from in this script, not where i actually clicked on
+    if (e.currentTarget.classList.contains('categorySlider__card')) {
+        const cardID = e.currentTarget.getAttribute('imageID');
+        cardInFocus = cardID;
+    }
+   
+    if (e.currentTarget === sliderArrowPrev) { 
+        cardInFocus -= 1;
+        cardCount += 200;
+        // changing the margin variable in css
+        document.documentElement.style.setProperty('--currentCard', cardCount + 'px');
+    }
+
+    if (e.currentTarget === sliderArrowNext) {
+        cardInFocus += 1;
+        cardCount -= 200;
+        document.documentElement.style.setProperty('--currentCard', cardCount + 'px');
+    }
+
+    console.log(cardCount);
+
+    // ----- changing focus card ------
+    console.log(cardInFocus);
+    // removing the focus class of the former focus card
+    const formerFocus = document.querySelector('.categorySlider__card.focus');
+    formerFocus.classList.remove('focus');
+
+    // looking for the card, that should be in focus now->looking for attribute with []
+    const newFocus = document.querySelector(`.categorySlider__card[imageID="${cardInFocus}"]`);
+    console.log(newFocus); 
+    newFocus.classList.toggle('focus');
 }
 
 
-// get buttons
-const SliderButtonNext = document.querySelector('.slider__arrow--next');
-const SliderButtonPrev = document.querySelector('.slider__arrow--previous');
+//change content of the page:
+/**
+ * just change main content (fetch from another html file)
+ * change background-image of hero section,
+ * change content of hero section (don't forget extra classes)
+ */
