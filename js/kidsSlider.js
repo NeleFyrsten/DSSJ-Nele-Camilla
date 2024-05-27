eb5def3c7e6caf52310c64c0af739568ca0d93ba
Camilla <cammal01@easv365.dk>
2024-05-27 13:48:06 +0200

    Added comments

--- js/kidsSlider.js
+++ js/kidsSlider.js
@@ -29,21 +29,26 @@ function addData(data){
         productCard.classList.add("kidsSlider__card");
         sliderContainer.appendChild(productCard);
 
+        //create the img element
         const productImage = document.createElement("img");
         productImage.src = item.image;
         productImage.classList.add("kidsSlider__image");
         productCard.appendChild(productImage);
 
+        //create the image credit
         const imageCredit = document.createElement("p");
         imageCredit.textContent = item.imageCredit;
         imageCredit.classList.add("imageCredit");
         productCard.appendChild(imageCredit);
 
+        //create the title
         const productTitle = document.createElement("h6");
         productTitle.textContent = item.title;
         productTitle.classList.add("kidsSlider__title");
         productCard.appendChild(productTitle);
 
+
+        //create the location
         const productDescription = document.createElement("p");
         productDescription.textContent = item.location;
         productDescription.classList.add("kidsSlider__location");
